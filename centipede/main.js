017db890c76103110b24751932c2616773cc24bd
Gunnar <gbt6@hi.is>
2023-11-11 00:12:24 +0000

    Added respawn of mushrooms

--- centipede/main.js
+++ centipede/main.js
@@ -1,15 +1,17 @@
 import * as THREE from 'three';
 
 let score = 0;
+let oldPlayerPosition = new THREE.Vector3(); // Initialize with a default position
 const lasers = []; // Array to keep track of lasers
 const mushrooms = []; // Array to keep track of mushrooms
-let oldPlayerPosition = new THREE.Vector3(); // Initialize with a default position
+const minMushrooms = 14; // Minimum number of mushrooms
+const maxMushrooms = 20; // Maximum number of mushrooms
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const camera2 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-
+// Function to toggle between camera and camera2
 let activeCamera = camera;
 function toggleCamera() {
     activeCamera = (activeCamera === camera) ? camera2 : camera;
@@ -18,9 +20,9 @@ function toggleCamera() {
         player.add(camera2);
 
         // Position the camera above and slightly behind the player, relative to the player
-        camera2.position.set(0, -1.5,1 ); // Position camera above (y = 1) and behind (z = 1) the player
+        camera2.position.set(0, -1.5,1 ); // Position camera above (y = -1.5) and behind (z = 1) the player
 
-        // Rotate the camera to look up along the Y-axis
+        // Rotate the camera to look up along the X-axis
         camera2.rotation.x = Math.PI / 2;
     } else {
         // When switching back to the main camera, detach camera2 from the player
@@ -29,11 +31,11 @@ function toggleCamera() {
     }
 }
 
+// Create canvas and set it's sise and positioning it in the middle of the window
 const renderer = new THREE.WebGLRenderer();
-// Create canvas and set it's sise in the middle of the window
-const multiplyer = 50;
-const canvasInnerWidth = 16 * multiplyer;
-const canvasInnerHeight = 15 * multiplyer;
+const multiplyer = 50; // keeps it within bounderies of my screen, you can change it
+const canvasInnerWidth = 16 * multiplyer; // 16 is given from teacher
+const canvasInnerHeight = 15 * multiplyer; // 15 is given from teacher
 renderer.setSize( canvasInnerWidth, canvasInnerHeight );
 renderer.domElement.style.position = 'absolute'; // Position the canvas element absolutely
 renderer.domElement.style.top = '50%'; // Align the top edge of the canvas to the vertical center of the screen
@@ -41,6 +43,9 @@ renderer.domElement.style.left = '50%'; // Align the left edge of the canvas to 
 renderer.domElement.style.transform = 'translate(-50%, -50%)'; // Offset the canvas by half of its width and height to center it
 document.body.appendChild( renderer.domElement );
 
+// Functions here are to create items inside the canvas
+
+// Creating the player
 const playerWidthX = 0.2;
 const playerHeightY = 0.1;
 const playerDeapthZ = 0.2;
@@ -56,6 +61,7 @@ scene.add( player );
 
 camera.position.z = 5;
 
+// Creating the mushrooms
 function createMushroom() {
     // Smaller radius for the mushroom cap
     const capRadius = 0.15; // Adjust this value to make the cap smaller
@@ -66,7 +72,7 @@ function createMushroom() {
 
     // Create mushroom cap (sphere)
     const capGeometry = new THREE.SphereGeometry(capRadius, 32, 32);
-    const capMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFF00 }); // Yellow color for the cap
+    const capMaterial = new THREE.MeshBasicMaterial({ color: 0xefcc00 }); // Yellow color for the cap
     const cap = new THREE.Mesh(capGeometry, capMaterial);
 
     // Create mushroom stem (cylinder)
@@ -75,7 +81,7 @@ function createMushroom() {
     const stem = new THREE.Mesh(stemGeometry, stemMaterial);
 
     // Position the cap on top of the stem
-    cap.position.y = stemHeight / 2; // Adjust so the cap is positioned correctly on the stem
+    cap.position.y = stemHeight / 2;
 
     // Create an empty object to group cap and stem
     const mushroom = new THREE.Object3D();
@@ -90,56 +96,40 @@ function createMushroom() {
     return mushroom;
 }
 
+// Function to create multiple mushrooms
 function createRandomMushrooms() {
-    const minMushrooms = 14; // Minimum number of mushrooms
-    const maxMushrooms = 20; // Maximum number of mushrooms
     const numMushrooms = Math.floor(Math.random() * (maxMushrooms - minMushrooms + 1)) + minMushrooms;
 
     // Canvas boundaries for x-coordinate
-    const minX = (-(canvasInnerWidth/100) / 2) - 2;
-    const maxX = ((canvasInnerWidth/100) / 2) + 2;
+    const minX = (-(canvasInnerWidth / 100) / 2) - 2;
+    const maxX = ((canvasInnerWidth / 100) / 2) + 2;
 
     // Canvas boundaries for y-coordinate, excluding the lowest 25%
-    const minY = (-canvasInnerHeight/100) / 4; // Start from 25% above the bottom
-    const maxY = (canvasInnerHeight/100) / 4;  // Up to the top
+    const minY = (-canvasInnerHeight / 100) / 4; // Start from 25% above the bottom
+    const maxY = (canvasInnerHeight / 100) * 0.4; // Up to last ~10% of the canvas
+
+    // Overlap threshold
+    const overlapThreshold = 0.5; // Adjust based on the size of your mushrooms
 
     for (let i = 0; i < numMushrooms; i++) {
+        let x, y, overlap;
+        do {
+            x = Math.random() * (maxX - minX) + minX;
+            y = Math.random() * (maxY - minY) + minY;
+            overlap = isOverlapping(x, y, mushrooms, overlapThreshold);
+        } while (overlap); // Keep generating new positions until no overlap is found
+
         const mushroom = createMushroom();
-
-        // Generate random positions within the designated area
-        const x = Math.random() * (maxX - minX) + minX;
-        const y = Math.random() * (maxY - minY) + minY;
-
-        // Set the mushroom's position
         mushroom.position.set(x, y, 0); // Z-coordinate is set to 0 (ground level)
         scene.add(mushroom);
-		mushrooms.push(mushroom); // Add each created mushroom to the array
+        mushrooms.push(mushroom); // Add each created mushroom to the array
     }
 }
 
 createRandomMushrooms();
 
-
-
 createRandomMushrooms();
 
-
-function animate() {
-	requestAnimationFrame( animate );
-    for (let i = lasers.length - 1; i >= 0; i--) {
-        let laser = lasers[i];
-        laser.position.y += 0.1; // Adjust speed as necessary
-
-
-    }
-	// Check to see if mushrooms and laser collide
-	checkLaserMushroomCollisions();
-	// Check and reset player position if out of bounds
-	checkBoundsAndResetPlayer();
-	renderer.render( scene, activeCamera );
-	const oldplayer = player.position;
-}
-animate();
 
 function createLaser() {
     const playerHeight = playerHeightY; // Assuming this is the player's height
@@ -154,6 +144,38 @@ function createLaser() {
     return laser;
 }
 
+// Function that adds more mushroom when they get down to too few
+function addMushrooms(numberToAdd) {
+    // Canvas boundaries for x-coordinate
+    const minX = (-(canvasInnerWidth / 100) / 2) - 2;
+    const maxX = ((canvasInnerWidth / 100) / 2) + 2;
+
+    // Canvas boundaries for y-coordinate, excluding the lowest 25%
+    const minY = (-canvasInnerHeight / 100) / 4; // Start from 25% above the bottom
+    const maxY = (canvasInnerHeight / 100) * 0.4;  // Up to last ~10% of the canvas
+
+	// Overlap threshold
+    const overlapThreshold = 0.5; // Adjust based on the size of your mushrooms
+
+    for (let i = 0; i < numberToAdd; i++) {
+        let x, y, overlap;
+        do {
+            x = Math.random() * (maxX - minX) + minX;
+            y = Math.random() * (maxY - minY) + minY;
+            overlap = isOverlapping(x, y, mushrooms, overlapThreshold);
+        } while (overlap); // Keep generating new positions until no overlap is found
+
+        const mushroom = createMushroom();
+        mushroom.position.set(x, y, 0); // Z-coordinate is set to 0 (ground level)
+        scene.add(mushroom);
+        mushrooms.push(mushroom); // Add each created mushroom to the array
+	}
+}
+
+
+// Functions to check for collitions
+
+// To make sure the player does not go out of bounds
 function checkBoundsAndResetPlayer() {
     // Define the boundaries of the canvas
 	const ciw = canvasInnerWidth / 100;
@@ -162,7 +184,7 @@ function checkBoundsAndResetPlayer() {
     const minX = (-ciw / 2) - 2;
     const maxX = (ciw / 2) + 2;
     const minY = -cih / 2;
-    const maxY = -2;
+    const maxY = -2; // Teacher does not want us to go far up
 
     // Check if the player is outside the boundaries
     if (player.position.x < minX || player.position.x > maxX || 
@@ -173,6 +195,7 @@ function checkBoundsAndResetPlayer() {
     }
 }
 
+// Check to see if you have hit a mushroom with a laser
 function checkLaserMushroomCollisions() {
     for (let i = lasers.length - 1; i >= 0; i--) {
         let laser = lasers[i];
@@ -185,6 +208,7 @@ function checkLaserMushroomCollisions() {
                     // Remove mushroom if hit 5 times
                     scene.remove(mushroom);
                     mushrooms.splice(j, 1);
+					replenishMushroomsIfNeeded();
                 } else {
                     // Reduce mushroom size by 20%
                     mushroom.scale.x *= 0.75;
@@ -206,11 +230,22 @@ function checkLaserMushroomCollisions() {
     }
 }
 
+// Helper function to check for collition on laser and mushroom
 function isCollision(laser, mushroom) {
     // Simple distance check (point-based collision)
     const distance = laser.position.distanceTo(mushroom.position);
     const collisionThreshold = 0.5; // Adjust as needed
     return distance < collisionThreshold;
+}
+
+// To check if a mushroom is placed at the same space as another
+function isOverlapping(newX, newY, existingMushrooms) {
+    for (const mushroom of existingMushrooms) {
+        if (Math.abs(mushroom.position.x - newX) < 0.01 && Math.abs(mushroom.position.y - newY) < 0.01) {
+            return true;
+        }
+    }
+    return false;
 }
 
 
@@ -242,6 +277,36 @@ function onDocumentKeyDown(event) {
 document.addEventListener("keydown", onDocumentKeyDown, false);
 document.querySelector('.toggle-button').addEventListener('click', toggleCamera);
 
+
+// Check if the mushroom count is low and add more
+function replenishMushroomsIfNeeded() {
+    const minimumMushrooms = 10; // Set the minimum number of mushrooms you want in the scene
+
+    if (mushrooms.length < minimumMushrooms) {
+        const mushroomsToAdd = Math.floor((Math.random() * (maxMushrooms - minMushrooms + 1)) + minMushrooms) - mushrooms.length;
+        addMushrooms(mushroomsToAdd);
+    }
+}
+
+
+// Updates score
 function updateScoreDisplay() {
     document.getElementById('scoreDisplay').innerText = 'Score: ' + score;
 }
+
+
+// Function to animate everything
+function animate() {
+	requestAnimationFrame( animate );
+    for (let i = lasers.length - 1; i >= 0; i--) {
+        let laser = lasers[i];
+        laser.position.y += 0.1; // Adjust speed as necessary
+    }
+	// Check to see if mushrooms and laser collide
+	checkLaserMushroomCollisions();
+	// Check and reset player position if out of bounds
+	checkBoundsAndResetPlayer();
+	renderer.render( scene, activeCamera );
+	const oldplayer = player.position;
+}
+animate();
